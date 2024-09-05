70ed3f5554cb082e655d6749372235de1d347551
opabhijeet <abhijeet.2201164ec@iiitbh.ac.in>
2024-09-05 16:41:26 +0530

    add logos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,13 @@ import {Header, Footer} from './index.js'
 import OfficeList from './OfficeList.jsx'
 import { AddPostOffice } from './index.js'
 import CardSlider from './CardSlider.jsx'
+import Logos from './Logos.jsx'
 
 function Home() {
   return (
     <>
     <div className='flex flex-col w-full items-center justify-center min-h-screen gap-10 mb-10'>
+      <Logos/>  
       <CardSlider/>
       <OfficeList/>
     </div>
