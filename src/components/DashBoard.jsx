e7ff686fece3c02c8ebbc10c989a7588203475f8
opabhijeet <abhijeet.2201164ec@iiitbh.ac.in>
2024-09-05 22:25:20 +0530

    fix footer

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -7,13 +7,7 @@ import GarbageDetectionTable from './DetectionTimeTable';
 
 const DashBoard = () => {
   return (
-    <div className='flex flex-col gap-4 h-screen bg-teal-900 text-gray-100 w-full'>
-      {/* Background */}
-      <div className='fixed inset-0 z-0'>
-        <div className='absolute inset-0 bg-gradient-to-br from-teal-900 via-teal-800 to-teal-900 opacity-80' />
-        <div className='absolute inset-0 backdrop-blur-sm' />
-      </div>
-
+    <div className='flex flex-col gap-4 min-h-fit bg-gradient-to-br from-teal-900 via-teal-800 to-teal-900 opacity-80 text-gray-100 w-full'>
       {/* Top Row */}
       <div className='flex flex-row gap-4 p-4'>
         <GarbageTimeSeriesChart className='flex-1 h-[360px]' />
