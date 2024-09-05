3f9b095d12d4c1ca068894c740350032b19a4efb
Souvik-Maity <souvik1252@gmail.com>
2024-09-06 03:14:36 +0530

    fixed_dashboard_styling

--- src/components/CleanlinessComplianceChart.jsx
+++ src/components/CleanlinessComplianceChart.jsx
@@ -12,12 +12,13 @@ const COLORS = ['#7c3aed', '#db2777'];
 const CleanlinessComplianceChart = () => {
   return (
     <motion.div
-      className='bg-teal-950 h-6/6 w-3/6 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 border border-gray-700'
+      className=' h-6/6 w-3/6 bg-opacity-50 backdrop-blur-md shadow-xl rounded-xl p-6 border border-gray-700'
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.5 }}
+      style={{backgroundColor:'#F5EDED'}}
     >
-      <h2 className='text-xl font-semibold text-gray-100 mb-10'>Post Office Cleanliness Compliance</h2>
+      <h2 className='text-xl font-semibold text-black mb-10'>Post Office Cleanliness Compliance</h2>
       <div className='h-[360px]'>
         <ResponsiveContainer width='100%' height='100%'>
           <PieChart>
@@ -37,10 +38,10 @@ const CleanlinessComplianceChart = () => {
             </Pie>
             <Tooltip
               contentStyle={{
-                backgroundColor: "rgba(31, 41, 55, 0.8)",
+                backgroundColor: "rgb(100, 130, 173)",
                 borderColor: "#4B5563",
               }}
-              itemStyle={{ color: "#E5E7EB" }}
+              itemStyle={{ color: "#000000" }}
             />
             <Legend
               iconType="circle"
