d6ae811512664d74190bd876e7debf91cfcfb6f5
Souvik-Maity <souvk1252@gmail.com>
2024-12-12 17:12:33 +0530

    push

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -12,6 +12,13 @@ function Home() {
   return (
     <>
     <div className='flex flex-col w-full items-center justify-center min-h-screen gap-10 mb-10'>
+    const images = [
+    "https://via.placeholder.com/800x300",
+    "https://via.placeholder.com/800x300/ff7f7f",
+    "https://via.placeholder.com/800x300/50b3a2",
+    "https://via.placeholder.com/800x300/d2a6ff",
+  ];
+      
       <Logos/>  
       <CardSlider/>
       <Modi/>
